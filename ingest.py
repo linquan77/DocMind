32b928aee2c49982a7f0f577d90d77e12aa0ecf7
周逸婷 <1415123803@qq.com>
2026-05-16 14:49:35 +0800

    refactor: 更新链式处理和依赖项，优化文档格式化

--- ingest.py
+++ ingest.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyMuPDFLoader, Docx2txtLoader, WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from config import *
